aeb0ee80fe7725b3c2b744995304878234f31569
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-04-18 21:58:51 +0300

    Implemented bindAndListen() and enhanced constructor

--- Trivia/Infrastructure/Communicator.cpp
+++ Trivia/Infrastructure/Communicator.cpp
@@ -1,40 +1,50 @@
 #include "Communicator.h"
 #include <iostream>
+#include <string>
 #include <WinSock2.h>
 
+using std::to_string;
 constexpr auto PORT = 7777;
 
 Communicator::Communicator()
 {
-	this->m_serverSocket = socket(AF_INET,  SOCK_STREAM,  IPPROTO_TCP); 
-
+	this->m_serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	
 	if (this->m_serverSocket == INVALID_SOCKET)
-		throw std::exception(__FUNCTION__ " - socket");
+		throw std::runtime_error(__FUNCTION__ " - socket() err: " + to_string(WSAGetLastError()));
 }
 
 void Communicator::bindAndListen() const
 {
-	struct sockaddr_in sa = { 0 };
+	struct sockaddr_in serverAddr{};
+	serverAddr.sin_family = AF_INET; // Must be AF_INET
+	serverAddr.sin_port = htons(PORT); // Port that server will listen for
+	serverAddr.sin_addr.s_addr = INADDR_ANY; // When there are few IPs for the machine. We will use always "INADDR_ANY"
 
-	sa.sin_port = htons(PORT); // port that server will listen for
-	sa.sin_family = AF_INET;   // must be AF_INET
-	sa.sin_addr.s_addr = INADDR_ANY; // when there are few IPs for the machine. We will use always "INADDR_ANY"
+	// Connects the socket and the configuration
+	if (bind(this->m_serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		throw std::runtime_error(__FUNCTION__ " - bind" + to_string(WSAGetLastError()));
 
-	// Connects between the socket and the configuration (port and etc..)
-	if (bind(m_serverSocket, (struct sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
-		throw std::exception(__FUNCTION__ " - bind");
+	// Start listening for incoming client requests
+	if (listen(this->m_serverSocket, SOMAXCONN) == SOCKET_ERROR)
+		throw std::runtime_error(__FUNCTION__ " - listen" + to_string(WSAGetLastError()));
 
-	// Start listening for incoming requests of clients
-	if (listen(m_serverSocket, SOMAXCONN) == SOCKET_ERROR)
-		throw std::exception(__FUNCTION__ " - listen");
-
-	std::cout << "Listening on port " << PORT << '\n'; // All good
+	std::cout << "Listening on port " << PORT << "...\n";
 }
 
+#include "WSAInitializer.h"
 int main(void)
 {
-	Communicator c;
-	c.bindAndListen();
+	try
+	{
+		WSAInitializer wsaInit;
+		Communicator c;
+		c.bindAndListen();
+	}
+	catch (const std::runtime_error& e)
+	{
+		std::cerr << e.what();
+	}
 
 	return 0;
 }
