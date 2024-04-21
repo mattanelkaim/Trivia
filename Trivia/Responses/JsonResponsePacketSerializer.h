70be49daa9ce7e472a16e90ef5766d84af1001ab
Gil Yelkin <gil.yelkin@gmail.com>
2024-04-22 02:30:07 +0300

    created JsonResponseSerializer class and ServerDefenitions.h

--- Trivia/Responses/JsonResponsePacketSerializer.h
+++ Trivia/Responses/JsonResponsePacketSerializer.h
@@ -1,6 +1,25 @@
 #pragma once
 
-static class JsonResponsePacketSerializer
-{
+#include <string>
+#include "../ServerDefenitions.h"
 
+// response structs
+struct ErrorResponse {
+	std::string message;
+};
+struct LoginResponse {
+	unsigned int status;
+};
+struct SignupResponse {
+	unsigned int status;
+};
+
+class JsonResponsePacketSerializer
+{
+public:
+	JsonResponsePacketSerializer() = delete; // this ensures that this class is never instanciated
+
+	static buffer serializeResponse(const ErrorResponse& response);
+	static buffer serializeResponse(const LoginResponse& response);
+	static buffer serializeResponse(const SignupResponse& response);
 };
