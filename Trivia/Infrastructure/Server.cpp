a75e627284c07a4a7708e64f553e5d2546ad8b10
Gil Yelkin <gil.yelkin@gmail.com>
2024-04-22 01:13:12 +0300

    fixed source file to how it should be based on v1.0.1

--- Trivia/Infrastructure/Server.cpp
+++ Trivia/Infrastructure/Server.cpp
@@ -1,6 +1,25 @@
 #include "Server.h"
+#include "./WSAInitializer.h"
+#include <iostream>
+
+constexpr std::string_view EXIT = "EXIT";
 
 void Server::run()
 {
+	try
+	{
+		WSAInitializer wsaInit;
+		Server server;
+		server.run();
 
+		std::string userInput;
+		do
+		{
+			std::cin >> userInput;
+		} while (userInput != EXIT);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what();
+	}
 }
--- Trivia/Source.cpp
+++ Trivia/Source.cpp
@@ -1,27 +1,10 @@
 #include "Infrastructure/Server.h"
-#include "Infrastructure/WSAInitializer.h"
-#include <iostream>
 
-constexpr std::string_view EXIT = "EXIT";
 
 int main(void)
 {
-	try
-	{
-		WSAInitializer wsaInit;
-		Server server;
-		server.run();
-
-		std::string userInput;
-		do
-		{
-			std::cin >> userInput;
-		} while (userInput != EXIT);
-	}
-	catch (const std::exception& e)
-	{
-		std::cerr << e.what();
-	}
+	Server server;
+	server.run();
 
 	return 0;
 }
