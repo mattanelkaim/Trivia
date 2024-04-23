cf9c8931154253435c32e8e3cfb474919d005a08
Gil Yelkin <gil.yelkin@gmail.com>
2024-04-23 17:37:52 +0300

    added defenitions and implemented all current JsonResponsePacketSerializer methods

--- Trivia/Responses/JsonResponsePacketSerializer.h
+++ Trivia/Responses/JsonResponsePacketSerializer.h
@@ -3,23 +3,12 @@
 #include <string>
 #include "../ServerDefenitions.h"
 
-// response structs
-struct ErrorResponse {
-	std::string message;
-};
-struct LoginResponse {
-	unsigned int status;
-};
-struct SignupResponse {
-	unsigned int status;
-};
-
 class JsonResponsePacketSerializer
 {
 public:
 	JsonResponsePacketSerializer() = delete; // this ensures that this class is never instanciated
 
-	static buffer serializeResponse(const ErrorResponse& response);
-	static buffer serializeResponse(const LoginResponse& response);
-	static buffer serializeResponse(const SignupResponse& response);
+	static buffer serializeErrorResponse(const ErrorResponse& response);
+	static buffer serializeLoginResponse(const LoginResponse& response);
+	static buffer serializeSignupResponse(const SignupResponse& response);
 };
--- Trivia/ServerDefenitions.h
+++ Trivia/ServerDefenitions.h
@@ -2,9 +2,38 @@
 
 #include <vector>
 
-#ifndef BYTE
-#define byte unsigned char
-#define BYTE
-#endif // !BYTE
+#ifndef BUFFER
+typedef std::vector<byte> buffer; // the 'byte' typedef already exists in <vector>
+#define BUFFER
+#endif // !BUFFER
 
-typedef std::vector<byte> buffer;
+
+#pragma region responseDefenitions
+
+// response structs
+struct ErrorResponse {
+	std::string message;
+};
+struct LoginResponse {
+	unsigned int status;
+};
+struct SignupResponse {
+	unsigned int status;
+};
+
+#define ERROR_MESSAGE_JSON "{message: \"ERROR\"}"
+#define ERROR_MESSAGE_JSON_LENGTH sizeof(ERROR_MESSAGE_JSON)
+
+#pragma endregion
+
+#pragma region protocolDefenitions
+
+enum messageType : byte
+{
+	RESPONSE,
+	REQUEST
+};
+
+#define NUM_BYTES_RESERVED_FOR_MESSAGE_LENGTH 4
+
+#pragma endregion
