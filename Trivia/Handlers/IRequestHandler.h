1a0979948a3a65c8af4b41b920396f291b9dba4e
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-04-27 00:19:57 +0300

    Function defenitions

--- Trivia/Handlers/IRequestHandler.h
+++ Trivia/Handlers/IRequestHandler.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "../ServerDefenitions.h"
+
+struct RequestResult; // Double-linkage shit
+
 class IRequestHandler
 {
-
+	virtual bool isRequestRelevant(const RequestInfo& info) = 0;
+	virtual RequestResult handleRequest(const RequestInfo& info) = 0;
 };
