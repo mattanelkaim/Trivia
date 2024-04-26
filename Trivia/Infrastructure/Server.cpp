510fa65fea62e24927c69a80a3dbb0b05691660d
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-04-26 21:59:19 +0300

    Removed unused includes

--- Trivia/Infrastructure/Server.cpp
+++ Trivia/Infrastructure/Server.cpp
@@ -1,15 +1,15 @@
 #include "Server.h"
+#include <iostream>
 #include <thread>
 
 constexpr std::string_view EXIT = "EXIT";
 
 void Server::run()
 {
-	std::string userInput;
-
 	std::thread t_connector(&Communicator::startHandleRequests, &(this->m_communicator));
 	t_connector.detach();
 
+	std::string userInput;
 	do
 	{
 		std::cin >> userInput;
