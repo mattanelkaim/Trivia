54da54fbb73e4471cdb8097ffe306bca008fad9f
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-04-27 16:47:38 +0300

    debugging

--- Trivia/Infrastructure/Helper.h
+++ Trivia/Infrastructure/Helper.h
@@ -7,12 +7,9 @@
 class Helper
 {
 public:
-    static std::string getStringPartFromSocket(SOCKET sc, const uint32_t& bytesNum);
+    static std::string getStringFromSocket(SOCKET sc, const uint32_t& bytesNum);
     static int getIntPartFromSocket(SOCKET sc, const uint32_t& bytesNum);
     static std::string getMessageFromSocket(SOCKET sc); // Helper function
     static void sendData(SOCKET sc, const std::string& message);
     static std::string getPaddedNumber(const size_t& num, const size_t& digits) noexcept;
-
-private:
-    static char* getPartFromSocket(SOCKET sc, const uint32_t& bytesNum, const int& flags = 0);
 };
