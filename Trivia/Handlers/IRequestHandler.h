a6fbd265cc2439c90eb56fcf895e99bb0c25ce76
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-04-27 04:38:05 +0300

    Declared virtual default destructor to resolve C5205 warning

--- Trivia/Handlers/IRequestHandler.h
+++ Trivia/Handlers/IRequestHandler.h
@@ -8,6 +8,10 @@ struct RequestInfo; // Double-circular-jerk-dependency-linkage mega-shit
 
 class IRequestHandler
 {
+public:
+	virtual ~IRequestHandler() = default;
+	
+private:
 	virtual bool isRequestRelevant(const RequestInfo& info) = 0;
 	virtual RequestResult handleRequest(const RequestInfo& info) = 0;
 };
