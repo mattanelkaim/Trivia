637f9c2e011823cfc747d585318b0d41e227051d
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-04-28 22:44:35 +0300

    Implemented LoginManager

---
+++ Trivia/Managers/LoginManager.cpp
@@ -0,0 +1,27 @@
+#include "LoggedUser.h"
+#include "LoginManager.h"
+
+
+bool LoginManager::isUserLoggedIn(const std::string& username) const noexcept
+{
+	auto isIdenticalUsername = [username](const LoggedUser& user) noexcept {return user.getUsername() == username; };
+	return std::find_if(m_loggedUsers.cbegin(), m_loggedUsers.cend(), isIdenticalUsername) != m_loggedUsers.cend();
+}
+
+void LoginManager::signup(const std::string& username, const std::string& password, const std::string& email)
+{
+	this->m_database->addNewUser(username, password, email);
+}
+
+void LoginManager::login(const std::string& username, const std::string& password)
+{
+	// Log in if username exists with correct password, and not already logged in
+	if (m_database->doesUserExist(username) && m_database->doesPasswordMatch(username, password) && !isUserLoggedIn(username))
+		this->m_loggedUsers.emplace_back(username); // Calls LoggedUser constructor
+}
+
+void LoginManager::logout(const std::string& username) noexcept
+{
+	auto isIdenticalUsername = [username](const LoggedUser& user) noexcept {return user.getUsername() == username; };
+	std::erase_if(this->m_loggedUsers, isIdenticalUsername);
+}
