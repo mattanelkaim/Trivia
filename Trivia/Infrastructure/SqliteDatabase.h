76d2a68acdf5d4762449f5dc8c324120fbf92e03
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-04-28 16:40:35 +0300

    changed arguments names and types

--- Trivia/Infrastructure/SqliteDatabase.h
+++ Trivia/Infrastructure/SqliteDatabase.h
@@ -9,16 +9,16 @@ public:
 
 	// queries
 	virtual bool doesUserExist(const std::string& username) const;
-	virtual bool doesPasswordMatch(const std::string&username , const std::string& password) const;
-	virtual void addNewUser(const std::string& username, const std::string& password, const std::string& mail);
+	virtual bool doesPasswordMatch(const std::string&username , const std::string_view& password) const;
+	virtual void addNewUser(const std::string& username, const std::string& password, const std::string& email);
 
 private:
 	sqlite3* _db;
 
 	// function to run queries on the databases
-	void runQuery(const char* query, const sqlite3_callback& callback, void* data) const;
+	void runQuery(const std::string_view& query, const sqlite3_callback& callback, void* data) const;
 
 	// callback functions
-	static int callbackInt(void* _data, int argc, char** argv, char** azColName) noexcept;
-	static int callbackText(void* _data, int argc, char** argv, char** azColName) noexcept;
+	static int callbackInt(void* _data, int argc, char** argv, char** columnsNames);
+	static int callbackText(void* _data, int argc, char** argv, char** columnsNames);
 };
