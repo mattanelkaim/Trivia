cbf3c5b0a7887cfe76e9842c18d267e09253ed5d
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-04-28 22:47:53 +0300

    string_view

--- Trivia/Managers/LoginManager.h
+++ Trivia/Managers/LoginManager.h
@@ -9,10 +9,10 @@ class LoginManager
 private:
 	IDatabase* m_database;
 	std::vector<LoggedUser> m_loggedUsers;
-	bool isUserLoggedIn(const std::string& username) const noexcept;
+	bool isUserLoggedIn(const std::string_view& username) const noexcept;
 
 public:
 	void signup(const std::string& username, const std::string& password, const std::string& email);
 	void login(const std::string& username, const std::string& password);
-	void logout(const std::string& username) noexcept;
+	void logout(const std::string_view& username) noexcept;
 };
