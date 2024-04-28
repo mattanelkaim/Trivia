2afbb970b75522e002742ecf6e83092b7ef957b8
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-04-28 22:44:23 +0300

    Added argument names

--- Trivia/Infrastructure/IDatabase.h
+++ Trivia/Infrastructure/IDatabase.h
@@ -10,6 +10,6 @@ public:
 
 	// queries
 	virtual bool doesUserExist(const std::string& username) const = 0;
-	virtual bool doesPasswordMatch(const std::string&, const std::string&) const = 0;
-	virtual void addNewUser(const std::string&, const std::string&, const std::string&) = 0;
+	virtual bool doesPasswordMatch(const std::string& username, const std::string& password) const = 0;
+	virtual void addNewUser(const std::string& username, const std::string& password, const std::string& email) = 0;
 };
