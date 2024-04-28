49addfafa1e4388c992376b05d55db5fb9c1ce88
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-04-28 17:30:50 +0300

    ready to merge

--- Trivia/Infrastructure/SqliteDatabase.h
+++ Trivia/Infrastructure/SqliteDatabase.h
@@ -8,7 +8,7 @@ public:
 	virtual bool open();	
 	virtual bool close();
 
-	// queries
+	// Queries
 	virtual bool doesUserExist(const std::string& username) const;
 	virtual bool doesPasswordMatch(const std::string&username , const std::string_view& password) const;
 	virtual void addNewUser(const std::string& username, const std::string& password, const std::string& email);
@@ -16,11 +16,11 @@ public:
 private:
 	sqlite3* _db;
 
-	// function to run queries on the databases
+	// Functions to run queries on the databases
 	void runQuery(const std::string_view& query) const;
 	void runQuery(const std::string_view& query, const sqlite3_callback& callback, void* data) const;
 
-	// callback functions
+	// Callback functions
 	static int callbackInt(void* _data, int argc, char** argv, char** columnsNames);
 	static int callbackText(void* _data, int argc, char** argv, char** columnsNames);
 };
