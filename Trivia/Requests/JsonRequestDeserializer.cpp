aaa16cd88dbfef53884d2354831c030c0fa0fc75
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-05-01 15:36:55 +0300

    nicer JSON error handling

--- Trivia/Requests/JsonRequestDeserializer.cpp
+++ Trivia/Requests/JsonRequestDeserializer.cpp
@@ -6,37 +6,47 @@
 
 using json = nlohmann::json;
 
+#include <iostream>
 LoginRequest JsonResponseDeserializer::deserializeLoginResponse(const buffer& response)
 {
-    if (response.size() < JSON_OFFSET + 2) // +2 curly braces
-        throw std::runtime_error("Response does not match protocol structure");
-
-    // Parse string msg to JSON
-    const std::string jsonMsg(response.cbegin() + JSON_OFFSET, response.cend());
-    json j = json::parse(jsonMsg);
-
-    // Store data from JSON on a LoginRequest struct
     LoginRequest request;
-    j.at("username").get_to(request.username);
-    j.at("password").get_to(request.password);
+
+    try
+    {
+        // Parse string msg to JSON
+        const json j = json::parse(response);
+        std::cout << j.dump() << '\n';
+
+        // Store data from JSON on a LoginRequest struct
+        j.at("username").get_to(request.username);
+        j.at("password").get_to(request.password);
+    }
+    catch (const json::parse_error& e)
+    {
+        throw std::runtime_error("ERROR parsing json response at byte " + e.byte);
+    }
 
     return request;
 }
 
 SignupRequest JsonResponseDeserializer::deserializeSignupResponse(const buffer& response)
 {
-    if (response.size() < JSON_OFFSET + 2) // +2 curly braces
-        throw std::runtime_error("Response does not match protocol structure");
-
-    // Parse string msg to JSON
-    const std::string jsonMsg(response.cbegin() + JSON_OFFSET, response.cend());
-    json j = json::parse(jsonMsg);
-
-    // Store data from JSON on a SignupRequest struct
     SignupRequest request;
-    j.at("username").get_to(request.username);
-    j.at("password").get_to(request.password);
-    j.at("email").get_to(request.email);
 
+    try
+    {
+        // Parse string msg to JSON
+        const json j = json::parse(response);
+
+        // Store data from JSON on a SignupRequest struct
+        j.at("username").get_to(request.username);
+        j.at("password").get_to(request.password);
+        j.at("email").get_to(request.email);
+    }
+    catch (const json::parse_error& e)
+    {
+        throw std::runtime_error("ERROR parsing json response at byte " + e.byte);
+    }
+    
     return request;
 }
