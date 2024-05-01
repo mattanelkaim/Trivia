ac0e5ff846bcaff2ed6e544486e7b69ce5e9a848
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-05-01 22:27:04 +0300

    implemented singletons

--- Trivia/Infrastructure/RequestHandlerFactory.cpp
+++ Trivia/Infrastructure/RequestHandlerFactory.cpp
@@ -1,15 +1,30 @@
 #include "../Handlers/LoginRequestHandler.h"
 #include "RequestHandlerFactory.h"
 
+// Singleton
+RequestHandlerFactory* m_HandlerFactory = nullptr;
+std::mutex RequestHandlerFactory::m_mutex;
+
+
+RequestHandlerFactory* RequestHandlerFactory::getInstance(IDatabase* db)
+{
+    std::lock_guard<std::mutex> lock(m_mutex);
+    if (m_HandlerFactory == nullptr)
+    {
+        m_HandlerFactory = new RequestHandlerFactory(db);
+    }
+    return m_HandlerFactory;
+}
+
 RequestHandlerFactory::RequestHandlerFactory(IDatabase* db)
-	: m_database(db), m_loginManager(LoginManager(db)) {}
+    : m_database(db), m_loginManager(LoginManager::getInstance(db)) {}
 
 LoginRequestHandler* RequestHandlerFactory::createLoginRequestHandler()
 {
-	return new LoginRequestHandler(*this);
+    return new LoginRequestHandler(*this);
 }
 
-LoginManager& RequestHandlerFactory::getLoginManager()
+LoginManager* RequestHandlerFactory::getLoginManager()
 {
-	return this->m_loginManager;
+    return this->m_loginManager;
 }
