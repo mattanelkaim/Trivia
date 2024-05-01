6047c6d046aee9f7afb34990f0ae3a834f8beefa
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-05-01 22:27:04 +0300

    implemented singletons

--- Trivia/Managers/LoginManager.h
+++ Trivia/Managers/LoginManager.h
@@ -2,22 +2,34 @@
 
 #include "../Infrastructure/IDatabase.h"
 #include "LoggedUser.h"
+#include <mutex>
 #include <string>
 #include <vector>
 
 class LoginManager
 {
 public:
-	explicit LoginManager(IDatabase* db);
-	LoginManager() = delete;
-
 	bool signup(const std::string& username, const std::string& password, const std::string& email);
 	bool login(const std::string& username, const std::string& password);
 	void logout(const std::string_view& username) noexcept;
 
+	// Singleton
+	LoginManager() = delete;
+	LoginManager(LoginManager& other) = delete;
+	void operator=(const LoginManager& other) = delete;
+	static LoginManager* getInstance(IDatabase* db);
+
 private:
+	// Members
 	IDatabase* m_database;
 	std::vector<LoggedUser> m_loggedUsers;
 
+	// Private methods
 	bool isUserLoggedIn(const std::string_view& username) const noexcept;
+
+	// Singleton
+	explicit LoginManager(IDatabase* db);
+	~LoginManager() = default;
+	inline static LoginManager* m_LoginManager = nullptr;
+	static std::mutex m_mutex;
 };
