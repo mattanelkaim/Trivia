9d5cd5b0f42180655ca6b3231e9ca17b714efae4
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-05-03 16:26:04 +0300

    Implemented some custom server exceptions

---
+++ Trivia/Other/IServerException.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+class IServerException : public std::exception
+{
+public:
+	IServerException() = default;
+	IServerException(const std::string& err) : m_err(err) {}
+	virtual const char* what() const override = 0;
+
+protected:
+	std::string m_err;
+};
+
