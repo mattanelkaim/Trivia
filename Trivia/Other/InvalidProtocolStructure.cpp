e6d67d715c229b2a2efaf04c2c4fde42790dd901
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-05-05 23:48:51 +0300

    constructor initializer lsit syntax

--- Trivia/Other/InvalidProtocolStructure.cpp
+++ Trivia/Other/InvalidProtocolStructure.cpp
@@ -1,10 +1,12 @@
 #include "InvalidProtocolStructure.h"
 
-InvalidProtocolStructure::InvalidProtocolStructure()
-	: IServerException("Invalid protocol structure!") {}
+InvalidProtocolStructure::InvalidProtocolStructure() :
+	IServerException("Invalid protocol structure!")
+{}
 
-InvalidProtocolStructure::InvalidProtocolStructure(const std::string& invalidPart)
-	: IServerException("Invalid protocol structure: " + invalidPart){}
+InvalidProtocolStructure::InvalidProtocolStructure(const std::string& invalidPart) :
+	IServerException("Invalid protocol structure: " + invalidPart)
+{}
 
 const char* InvalidProtocolStructure::what() const
 {
