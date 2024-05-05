1ffd42e09ead7bfd1b32984d1e0fdb864b23fea2
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-05-05 23:48:51 +0300

    constructor initializer lsit syntax

--- Trivia/Other/UnexpectedClientExit.cpp
+++ Trivia/Other/UnexpectedClientExit.cpp
@@ -1,7 +1,7 @@
 #include "UnexpectedClientExit.h"
 
-UnexpectedClientExit::UnexpectedClientExit(const SOCKET crashedClient)
-	: IServerException("Client socket " + std::to_string(crashedClient) + " unexpectedly disconnected")
+UnexpectedClientExit::UnexpectedClientExit(const SOCKET crashedClient) :
+	IServerException("Client socket " + std::to_string(crashedClient) + " unexpectedly disconnected")
 {}
 
 const char* UnexpectedClientExit::what() const
