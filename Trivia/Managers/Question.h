aa3c968d50b79a68270500c3a8106a18e796afce
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-05-13 01:54:32 +0300

    deleted default constructor

--- Trivia/Managers/Question.h
+++ Trivia/Managers/Question.h
@@ -6,9 +6,10 @@
 class Question 
 {
 public:
-    //C'tor
-    Question() = default;
+    // Constructors
+    Question() = delete;
     Question(const std::string& question, const std::vector<std::string>& answers);
+
     inline const std::string& getQuestion() const;
     inline const std::vector<std::string>& getPossibleAnswers() const;
     inline const std::string& getCorrectAnswer() const; // The correct answer will always be the first one
