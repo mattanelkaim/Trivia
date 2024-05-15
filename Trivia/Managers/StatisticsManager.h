2faeb361a300df2039c49842fed55e42ec01e6b4
Gil_Yelkin <gil.yelkin@gmail.com>
2024-05-15 19:39:13 +0300

    tov muttuny bunny

--- Trivia/Managers/StatisticsManager.h
+++ Trivia/Managers/StatisticsManager.h
@@ -3,15 +3,20 @@
 #include "IDatabase.h"
 #include <string>
 #include <vector>
+#include <mutex>
+#include <memory>
 
 class StatisticsManager
 {
+friend std::unique_ptr<StatisticsManager>;
+
 public:
     // Singelton
+    ~StatisticsManager() noexcept = default;
     StatisticsManager() = delete;
     StatisticsManager(const StatisticsManager& other) = delete;
     void operator=(const StatisticsManager& other) = delete;          
-    static StatisticsManager* getInstance(IDatabase* db);
+    static std::unique_ptr<StatisticsManager>& getInstance(IDatabase* db);
 
     std::vector<std::string> getHighScore() const;
     std::vector<std::string> getUserStatistics(const std::string& username) const;
@@ -20,7 +25,6 @@ private:
     IDatabase* m_database;    
     // Singleton
     explicit StatisticsManager(IDatabase* db);
-    ~StatisticsManager() noexcept = default;
-    inline static StatisticsManager* m_StatisticsManager = nullptr;
+    inline static std::unique_ptr<StatisticsManager> m_StatisticsManager = nullptr;
     inline static std::mutex m_mutex;
 };
