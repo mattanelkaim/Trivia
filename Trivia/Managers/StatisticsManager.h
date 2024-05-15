4df5d524225cedc410a7f2c774a8e82c40379344
Gil Yelkin <gil.yelkin@gmail.com>
2024-05-15 14:23:07 +0300

    made StatisticsManager into a Singelton

--- Trivia/Managers/StatisticsManager.h
+++ Trivia/Managers/StatisticsManager.h
@@ -7,12 +7,20 @@
 class StatisticsManager
 {
 public:
+    // Singelton
     StatisticsManager() = delete;
-    StatisticsManager(IDatabase* db);
+    StatisticsManager(const StatisticsManager& other) = delete;
+    void operator=(const StatisticsManager& other) = delete;          
+    static StatisticsManager* getInstance(IDatabase* db);
 
     std::vector<std::string> getHighScore() const;
     std::vector<std::string> getUserStatistics(const std::string& username) const;
 
 private:
-    IDatabase* m_database;
+    IDatabase* m_database;    
+    // Singleton
+    explicit StatisticsManager(IDatabase* db);
+    ~StatisticsManager() noexcept = default;
+    inline static StatisticsManager* m_StatisticsManager = nullptr;
+    inline static std::mutex m_mutex;
 };
