1b96a84d487127e15a3b8f926a2590a0722276d8
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-05-18 23:33:49 +0300

    fixed warnings

--- Trivia/Handlers/LoginRequestHandler.cpp
+++ Trivia/Handlers/LoginRequestHandler.cpp
@@ -5,7 +5,6 @@
 #include "MenuRequestHandler.h"
 #include "RequestHandlerFactory.h"
 #include "ServerDefinitions.h"
-#include <memory>
 #include <stdexcept>
 
 
@@ -40,14 +39,14 @@ RequestResult LoginRequestHandler::login(const RequestInfo& info)
     const auto request = JsonRequestDeserializer::deserializeRequest<LoginRequest>(info.buffer);
 
     LoginManager* loginManager = this->m_handlerFactory.getLoginManager();
-    [[likely]] if (loginManager->login(request.username, request.password))
+    if (loginManager->login(request.username, request.password)) [[likely]]
     {
         return RequestResult{
             .response = JsonResponseSerializer::serializeResponse(LoginResponse{RESPONSE}),
             .newHandler = new MenuRequestHandler()
         };
     }
-    else
+    else [[unlikely]]
     {
         return RequestResult{
             .response = JsonResponseSerializer::serializeResponse(ErrorResponse{"Login failed"}),
@@ -61,14 +60,14 @@ RequestResult LoginRequestHandler::signup(const RequestInfo& info)
     const auto request = JsonRequestDeserializer::deserializeRequest<SignupRequest>(info.buffer);
 
     LoginManager* loginManager = this->m_handlerFactory.getLoginManager();
-    [[likely]] if (loginManager->signup(request.username, request.password, request.email))
+    if (loginManager->signup(request.username, request.password, request.email)) [[likely]]
     {
         return RequestResult{
             .response = JsonResponseSerializer::serializeResponse(SignupResponse{RESPONSE}),
             .newHandler = new MenuRequestHandler()
         };
     }
-    else
+    else [[unlikely]]
     {
         return RequestResult{
             .response = JsonResponseSerializer::serializeResponse(ErrorResponse{"Signup failed"}),
