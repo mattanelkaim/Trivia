79261f930055a47e6f89690c6385ea6e88a47bf1
Gil_Yelkin <gil.yelkin@gmail.com>
2024-05-18 19:14:57 +0300

    renamed file extension to hpp

--- Trivia/Requests/JsonRequestDeserializer.hpp
+++ Trivia/Requests/JsonRequestDeserializer.hpp
@@ -1,35 +1,39 @@
 #pragma once
 
 #include "ServerDefinitions.h"
+#include "json.hpp"
 
-class JsonResponseDeserializer
+using json = nlohmann::json;
+
+class JsonRequestDeserializer
 {
 public:
-    JsonResponseDeserializer() = delete; // This ensures that this class is never instantiated
+    JsonRequestDeserializer() = delete; // This ensures that this class is never instantiated
 
     template <typename T>
     static T deserializeRequest(const readonly_buffer requestBuffer)
     {
         T request;
 
-        try {
+        try 
+        {
             // Parse string msg to JSON
             const json j = json::parse(requestBuffer);
 
             // Store data from JSON on a struct
-            if constexpr (std::is_same_v<T, LoginRequest> || std::is_same_v<T, SignupRequest) 
+            if constexpr (std::is_same_v<T, LoginRequest> || std::is_same_v<T, SignupRequest>) 
             {
                 j.at("username").get_to(request.username);
                 j.at("password").get_to(request.password);
 
-                if constexpr (std::is_same_v<T, Signup>)
+                if constexpr (std::is_same_v<T, SignupRequest>)
                 {
                     j.at("email").get_to(request.email);
                 }
             }
             else if constexpr (std::is_same_v<T, GetPlayersInRoomRequest> || std::is_same_v<T, JoinRoomRequest>)
             {
-                j.at("roomId").get_to(request.roomId)
+                j.at("roomId").get_to(request.roomId);
             }
             else if constexpr (std::is_same_v<T, CreateRoomRequest>) /* it is OK to use "else if" in this case instead of just "else" because this whole if - else block is precompiled,
                                                                         also it is more readable like this */
@@ -39,11 +43,17 @@ public:
                 j.at("questionCount").get_to(request.questionCount);
                 j.at("answerTimeout").get_to(request.answerTimeout);
             }
+            else
+            {
+                throw std::runtime_error("not supported yet");
+            }
         }
-        catch (const json::parse_error& e) {
+        catch (const json::parse_error& e) 
+        {
             throw std::runtime_error("ERROR parsing JSON response at byte " + std::to_string(e.byte));
         }
-        catch (const json::out_of_range& e) {
+        catch (const json::out_of_range& e) 
+        {
             throw std::runtime_error(std::string("ERROR parsing JSON: ") + e.what());
         }
 
