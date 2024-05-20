16b3fdb87c3aafcb35d5092be2844545bea610f1
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-05-20 03:51:01 +0300

    simplified reading from json object, returning an immidietly-constructed T

--- Trivia/Requests/JsonRequestDeserializer.hpp
+++ Trivia/Requests/JsonRequestDeserializer.hpp
@@ -18,37 +18,43 @@ namespace JsonRequestDeserializer
      * @return The deserialized request object.
      * @throws std::runtime_error if there is an error parsing the JSON or if the request type is not supported.
      */
+
     template <std::derived_from<Request> T>
     T deserializeRequest(const readonly_buffer requestBuffer)
     {
-        T request{};
-
         try
         {
             // Parse string msg to JSON
             const json j = json::parse(requestBuffer);
 
             // Store data from JSON on a struct
-            if constexpr (std::same_as<T, LoginRequest> || std::same_as<T, SignupRequest>)
+            if constexpr (std::same_as<T, LoginRequest>)
             {
-                j.at("username").get_to(request.username);
-                j.at("password").get_to(request.password);
-
-                if constexpr (std::same_as<T, SignupRequest>)
-                {
-                    j.at("email").get_to(request.email);
-                }
+                return T{
+                    .username = j["username"],
+                    .password = j["password"]
+                };
+            }
+            else if constexpr (std::same_as<T, SignupRequest>)
+            {
+                return T{
+                    .username = j["username"],
+                    .password = j["password"],
+                    .email = j["email"]
+                };
             }
             else if constexpr (std::same_as<T, CreateRoomRequest>)
             {
-                j.at("roomName").get_to(request.roomName);
-                j.at("maxUsers").get_to(request.maxUsers);
-                j.at("questionCount").get_to(request.questionCount);
-                j.at("answerTimeout").get_to(request.answerTimeout);
+                return T{
+                    .roomName = j["roomName"],
+                    .maxUsers = j["maxUsers"],
+                    .questionCount = j["questionCount"],
+                    .answerTimeout = j["answerTimeout"]
+                };
             }
             else if constexpr (requires{ T::roomId; }) // Either GetPlayersInRoomRequest or JoinRoomRequest
             {
-                j.at("roomId").get_to(request.roomId);
+                return T{.roomId = j["roomId"]};
             }
             else
             {
@@ -63,7 +69,5 @@ namespace JsonRequestDeserializer
         {
             throw std::runtime_error(std::string("ERROR parsing JSON: ") + e.what());
         }
-
-        return request;
     }
-};
+}; // namespace JsonRequestDeserializer
