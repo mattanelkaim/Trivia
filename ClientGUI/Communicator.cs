09c28394cc82f6180582a2b1bffda6c7de198175
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-05-22 23:06:06 +0300

    started to implement communicator

---
+++ ClientGUI/Communicator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ClientGUI
+{
+    internal class Communicator
+    {
+        static private TcpClient client;
+        static private NetworkStream stream;
+        static private string IP = "127.0.0.0";
+        static private short port = 7777;
+
+        public static void Connect()
+        {
+            client = new TcpClient(IP, port);
+            stream = client.GetStream();
+        }
+    }
+}
