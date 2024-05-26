d3a5a43184fcaf5fd0a9cfddf41f18e1ffec94b7
Gil_Yelkin <gil.yelkin@gmail.com>
2024-05-26 16:21:03 +0300

    finished implementing all ManuRequestHandler methods

--- Trivia/Managers/RoomManager.cpp
+++ Trivia/Managers/RoomManager.cpp
@@ -18,6 +18,7 @@ void RoomManager::createRoom(const LoggedUser& user, const RoomData& data)
         throw std::runtime_error("Room with id " + std::to_string(data.id) + " already exists");
 
     emplaced.first->second.addUser(user); // Add to room the room creator
+    this->roomIdCounter++;
 }
 
 void RoomManager::deleteRoom(const uint32_t roomId) noexcept
--- Trivia/Managers/RoomManager.h
+++ Trivia/Managers/RoomManager.h
@@ -14,9 +14,11 @@ class RoomManager final
 public:
     void createRoom(const LoggedUser& user, const RoomData& data);
     void deleteRoom(uint32_t roomId) noexcept;
+    
     uint32_t getRoomState(uint32_t roomId) const;
     std::vector<RoomData> getRooms() const;
     Room& getRoom(uint32_t roomId);
+    inline static uint32_t getNextRoomId() { return roomIdCounter + 1; };
 
     // Singleton
     RoomManager(RoomManager& other) = delete;
@@ -26,7 +28,7 @@ public:
 
 private:
     std::unordered_map<uint32_t, Room> m_rooms;
-
+    inline static uint32_t roomIdCounter = 0;
     // Singleton
     RoomManager() noexcept = default;
     inline static std::shared_ptr<RoomManager> m_RoomManager = nullptr;
