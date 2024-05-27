27342aee61087ff734078175979b94f3498e3dfe
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-05-28 00:29:17 +0300

    Basic login form

--- ClientGUI/MainWindow.xaml.cs
+++ ClientGUI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.Json;
+using System.Windows.Media.Animation;
 
 namespace ClientGUI
 {
@@ -18,6 +19,7 @@ namespace ClientGUI
         {
             InitializeComponent();
             this.LoginSubmit.Click += new RoutedEventHandler(LoginSubmit_Click);
+
 
             try
             {
@@ -31,7 +33,7 @@ namespace ClientGUI
 
         private void LoginSubmit_Click(object sender, RoutedEventArgs e)
         {
-            string json = JsonSerializer.Serialize(new { username = this.Username.Text, password = this.Password.Text });
+            string json = JsonSerializer.Serialize(new { username = this.UsernameField.Text, password = this.PasswordField.Password });
 
             string msg = Helper.Serialize(json, Helper.MessageType.Login);
             MessageBox.Show("[Sending]: " + msg);
@@ -39,6 +41,56 @@ namespace ClientGUI
 
             string response = Communicator.Receive();
             MessageBox.Show("[Received]: " + response);
+        }
+
+        private void Username_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ThicknessAnimation animation = new()
+            {
+                To = new Thickness(2, 0, 0, 0),
+                Duration = TimeSpan.FromMilliseconds(200),
+                EasingFunction = new QuarticEase()
+            };
+            this.UsernameTextBlock.BeginAnimation(MarginProperty, animation);
+        }
+
+        private void Username_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (this.UsernameField.Text.Length == 0)
+            {
+                ThicknessAnimation animation = new()
+                {
+                    To = new Thickness(23, 17, 0, 0),
+                    Duration = TimeSpan.FromMilliseconds(200),
+                    EasingFunction = new QuarticEase()
+                };
+                this.UsernameTextBlock.BeginAnimation(MarginProperty, animation);
+            }
+        }
+
+        private void Password_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ThicknessAnimation animation = new()
+            {
+                To = new Thickness(2, 47, 0, 0),
+                Duration = TimeSpan.FromMilliseconds(200),
+                EasingFunction = new QuarticEase()
+            };
+            this.PasswordTextBlock.BeginAnimation(MarginProperty, animation);
+        }
+
+        private void Password_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (this.PasswordField.Password.Length == 0)
+            {
+                ThicknessAnimation animation = new()
+                {
+                    To = new Thickness(23, 64, 0, 0),
+                    Duration = TimeSpan.FromMilliseconds(200),
+                    EasingFunction = new QuarticEase()
+                };
+                this.PasswordTextBlock.BeginAnimation(MarginProperty, animation);
+            }
         }
     }
 }
