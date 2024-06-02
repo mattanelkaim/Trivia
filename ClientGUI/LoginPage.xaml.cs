0ce2826ab77078f8e2bb8992732f1ee66950799a
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-06-03 01:48:24 +0300

    fixed bug

--- ClientGUI/LoginPage.xaml.cs
+++ ClientGUI/LoginPage.xaml.cs
@@ -64,7 +64,7 @@ namespace ClientGUI
                 {
                     textBlock = this.UsernameTextBlock;
                 }
-                else if (textBox.Text.Contains("Password"))
+                else if (textBox.Name.Contains("Password"))
                 {
                     textBlock = this.PasswordTextBlock;
                 }
@@ -108,7 +108,7 @@ namespace ClientGUI
                     textBlock = this.UsernameTextBlock;
                     fieldValue = Username;
                 }
-                else if (textBox.Text.Contains("Password"))
+                else if (textBox.Name.Contains("Password"))
                 {
                     textBlock = this.PasswordTextBlock;
                     fieldValue = Password;
