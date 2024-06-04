97e617db692422f4306c20a247116467957201d0
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-06-05 00:45:39 +0300

    using variables in PersonalStats

--- ClientGUI/PersonalStatsPage.xaml.cs
+++ ClientGUI/PersonalStatsPage.xaml.cs
@@ -20,8 +20,27 @@ namespace ClientGUI
     /// </summary>
     public partial class PersonalStatsPage : Page
     {
+        public float Score { get; set; }
+        public uint Games { get; set; }
+        public uint TotalAnswers { get; set; }
+        public uint CorrectAnswers { get; set; }
+        public uint WrongAnswers => TotalAnswers - CorrectAnswers;
+        public float AccuracyStat { get; set; } // Accuracy is a reserved keyword
+        public string AccuracyStr => AccuracyStat.ToString("P2"); // To percentage with max 2 decimal places
+
         public PersonalStatsPage()
         {
+            DataContext = this;
+            this.Score = 0;
+            this.Games = 0;
+            this.TotalAnswers = 11;
+            this.CorrectAnswers = 7;
+
+            if (TotalAnswers != 0)
+                this.AccuracyStat = (float)CorrectAnswers / TotalAnswers;
+            else
+                this.AccuracyStat = 0;
+
             InitializeComponent();
         }
         private void HomeButton_Click(object sender, RoutedEventArgs? e)
