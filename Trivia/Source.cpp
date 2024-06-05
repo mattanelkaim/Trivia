c488e5f35cbdfa5cae20bc50473ff9dade666dab
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-06-06 00:53:48 +0300

    added dummy rooms for later testing

--- Trivia/Source.cpp
+++ Trivia/Source.cpp
@@ -10,10 +10,43 @@ Things to do before shipping
 #include <exception>
 #include <iostream>
 
+#include "RoomManager.h"
+static void createDummyRooms()
+{
+    RoomManager::getInstance()->createRoom("admin", {
+        .name = "Room1",
+        .id = RoomManager::getNextRoomId(),
+        .maxPlayers = 6,
+        .numOfQuestionsInGame = 12,
+        .timePerQuestion = 10,
+        .status = RoomStatus::OPEN
+    });
+
+    RoomManager::getInstance()->createRoom("adm2", {
+        .name = "Room2",
+        .id = RoomManager::getNextRoomId(),
+        .maxPlayers = 10,
+        .numOfQuestionsInGame = 7,
+        .timePerQuestion = 30,
+        .status = RoomStatus::CLOSED
+    });
+
+    RoomManager::getInstance()->createRoom("mattan", {
+        .name = "Room3",
+        .id = RoomManager::getNextRoomId(),
+        .maxPlayers = 5,
+        .numOfQuestionsInGame = 10,
+        .timePerQuestion = 20,
+        .status = RoomStatus::OPEN
+    });
+}
+
 int main()
 {
     try
     {
+        createDummyRooms(); // TODO remove
+
         const WSAInitializer wsaInit;
         Server::getInstance()->run();
     }
