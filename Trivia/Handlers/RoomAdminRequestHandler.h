6b70241c13f081d463c425d7e36bb0eecdd71f1d
Gil_Yelkin <gil.yelkin@gmail.com>
2024-06-11 12:34:24 +0300

    added all basic properties of the new classes

---
+++ Trivia/Handlers/RoomAdminRequestHandler.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "IRequestHandler.h"
+#include "Room.h"
+#include "RoomManager.h"
+#include "RequestHandlerFactory.h"
+#include "LoggedUser.h"
+#include "IDatabase.h"
+
+class RoomAdminRequestHandler : public IRequestHandler
+{
+public:
+	explicit RoomAdminRequestHandler(IDatabase* db, const LoggedUser user, Room room);
+
+private:
+	Room m_room;
+	const LoggedUser m_user;
+	RoomManager* m_roomManager;
+	RequestHandlerFactory* m_handlerFactory;
+};
