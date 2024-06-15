dbf4f094ab47b2d124b0c8699fda3845821b49a7
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-06-16 02:45:55 +0300

    excluded ctre.hpp from static tests

--- Trivia/cppcheck_test.bat
+++ Trivia/cppcheck_test.bat
@@ -1,2 +1,2 @@
-cppcheck --enable=all --suppress-xml=cppcheck.xml --project=Trivia.sln --suppress=*:Other/json.hpp --suppress=*:Other/sqlite3.h -q
+cppcheck --enable=all --suppress-xml=cppcheck.xml --project=Trivia.sln --suppress=*:Other/json.hpp --suppress=*:Other/ctre.hpp --suppress=*:Other/sqlite3.h -q
 pause
