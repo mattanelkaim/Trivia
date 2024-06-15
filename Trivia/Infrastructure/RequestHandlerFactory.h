8a3a62073c2b34da9c5ac952d9380dbed3ac9c68
Gil_Yelkin <gil.yelkin@gmail.com>
2024-06-15 22:19:29 +0300

    turned RequestHandlerFactory into a namespace instead of a singleton class

--- Trivia/Infrastructure/RequestHandlerFactory.h
+++ Trivia/Infrastructure/RequestHandlerFactory.h
@@ -2,34 +2,13 @@
 
 #include "LoggedUser.h"
 #include "MenuRequestHandler.h"
+#include "Room.h"
 #include "RoomAdminRequestHandler.h"
 #include "RoomMemberRequestHandler.h"
 
-class RequestHandlerFactory final
+namespace RequestHandlerFactory
 {
-public:
-    /*######################################
-    ############ PUBLIC METHODS ############
-    ######################################*/
-
-    static MenuRequestHandler* createMenuRequestHandler(const LoggedUser& user);
-    static RoomAdminRequestHandler* createRoomAdminRequestHandler(const LoggedUser& user, Room room);
-    static RoomMemberRequestHandler* createRoomMemberRequestHandler(const LoggedUser& user, Room room);
-
-
-    /*######################################
-    ############### SINGLETON ##############
-    ######################################*/
-
-    RequestHandlerFactory(const RequestHandlerFactory& other) = delete;
-    void operator=(const RequestHandlerFactory& other) = delete;
-    static RequestHandlerFactory& getInstance() noexcept;
-    ~RequestHandlerFactory() noexcept = default;
-
-private:
-    /*######################################
-    ############### SINGLETON ##############
-    ######################################*/
-
-    RequestHandlerFactory() noexcept = default;
-};
+	MenuRequestHandler* createMenuRequestHandler(const LoggedUser& user);
+	RoomAdminRequestHandler* createRoomAdminRequestHandler(const LoggedUser& user, Room room);
+	RoomMemberRequestHandler* createRoomMemberRequestHandler(const LoggedUser& user, Room room);
+}; // namespace RequestHandlerFactory
