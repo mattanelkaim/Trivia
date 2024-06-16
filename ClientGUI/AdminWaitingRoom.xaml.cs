dadd2d11a08b13fed5d01af34a1b5bc55c698dbc
Gil_Yelkin <gil.yelkin@gmail.com>
2024-06-17 01:20:57 +0300

    created AdminWaitingRoomPage

---
+++ ClientGUI/AdminWaitingRoom.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientGUI
+{
+    /// <summary>
+    /// Interaction logic for AdminWaitingRoom.xaml
+    /// </summary>
+    public partial class AdminWaitingRoom : Page
+    {
+        public AdminWaitingRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseRoom_Click(object sender, RoutedEventArgs? e)
+        {
+            Helper.ResponseType status = Helper.SendCloseRoomRequest(); 
+
+            switch (status)
+            {
+                case Helper.ResponseType.OK:
+                    this.NavigationService.Navigate(new MenuPage());
+                    break;
+                default:
+                    MessageBox.Show("Cannot Close room!");
+                    break;
+            }
+        }
+
+        private void StartGame_Click(object sender, RoutedEventArgs? e)
+        {
+            Helper.ResponseType status = Helper.SendStartGameRequest();
+
+            switch (status)
+            {
+                case Helper.ResponseType.OK:
+                    // this.NavigationService.Navigate(new AdminGamePage());
+                    break;
+                default:
+                    MessageBox.Show("Cannot start room!");
+                    break;
+            }
+        }
+    }
+}
