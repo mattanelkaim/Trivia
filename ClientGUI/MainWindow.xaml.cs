2914499378851f90567c7ca9f84f690f773b52a4
Gil_Yelkin <gil.yelkin@gmail.com>
2024-06-16 21:52:36 +0300

    created loading page page

--- ClientGUI/MainWindow.xaml.cs
+++ ClientGUI/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace ClientGUI
 
             try
             {
-                Communicator.Connect();
+                //Communicator.Connect();
             }
             catch (Exception e)
             {
