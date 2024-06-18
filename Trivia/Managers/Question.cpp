72a35c7665fb3b9aac21e351c7ddd09f549bd35a
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-06-19 02:30:02 +0300

    Question constructor now shuffles the recieves answers

--- Trivia/Managers/Question.cpp
+++ Trivia/Managers/Question.cpp
@@ -1,5 +1,7 @@
 #include "Question.h"
+#include <algorithm> // std::shuffle
 #include <cstdint>
+#include <random>
 #include <string>
 #include <utility> // std::move
 #include <vector>
@@ -8,7 +10,29 @@ Question::Question(std::string question, std::vector<std::string> answers) noexc
     m_question(std::move(question)),
     m_possibleAnswers(std::move(answers)),
     m_correctAnswerId(0)
-{}
+{
+    const std::string correctAnswer = this->m_possibleAnswers.front(); // The correct answer is the first one (in DB)
+
+    try
+    {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::shuffle(m_possibleAnswers.begin(), m_possibleAnswers.end(), gen);
+
+        // Find the correct answer's new index
+        for (uint8_t i = 0; const std::string& answer : m_possibleAnswers)
+        {
+            if (answer == correctAnswer)
+            {
+                m_correctAnswerId = i;
+                return;
+            }
+            ++i;
+        }
+    }
+    catch (...) // If shuffle fails, we will just use the original order
+    {}
+}
 
 const std::string& Question::getQuestion() const noexcept
 {
