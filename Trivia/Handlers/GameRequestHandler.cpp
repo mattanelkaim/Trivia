4053698218ac241183e2c4eaa4cd4cf37d85694e
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-06-18 03:36:38 +0300

    implemented some more

--- Trivia/Handlers/GameRequestHandler.cpp
+++ Trivia/Handlers/GameRequestHandler.cpp
@@ -1,1 +1,51 @@
+#pragma warning(disable: 4061) // enumerator in switch of enum is not explicitly handled by a case label
+
+#include "Game.h"
 #include "GameRequestHandler.h"
+#include "Helper.h"
+#include "JsonResponseSerializer.h"
+#include "LoggedUser.h"
+#include "ServerDefinitions.h"
+#include "ServerException.h"
+#include <utility> // std::move
+#if SERVER_DEBUG
+#include <iostream>
+#endif
+
+
+GameRequestHandler::GameRequestHandler(LoggedUser user, Game& game) noexcept :
+    m_game(game),
+    m_user(std::move(user))
+{}
+
+bool GameRequestHandler::isRequestRelevant(const RequestInfo& info) const noexcept
+{
+    switch (info.id)
+    {
+    case SUBMIT_ANSWER:
+    case LEAVE_GAME:
+    case GET_QUESTION:
+    case GET_GAME_RESULTS:
+        return true;
+    default:
+        return false;
+    }
+}
+
+RequestResult GameRequestHandler::handleRequest(const RequestInfo& info) noexcept
+{
+    try
+    {
+
+    }
+    catch (const ServerException& e)
+    {
+        if constexpr (SERVER_DEBUG)
+            std::cerr << ANSI_RED << Helper::formatError(__FUNCTION__, e.what()) << ANSI_NORMAL << '\n';
+
+        return RequestResult{
+            .response = JsonResponseSerializer::serializeResponse(ErrorResponse{"Invalid protocol structure"}),
+            .newHandler = nullptr
+        };
+    }
+}
--- Trivia/Infrastructure/RequestHandlerFactory.cpp
+++ Trivia/Infrastructure/RequestHandlerFactory.cpp
@@ -1,3 +1,5 @@
+#include "Game.h"
+#include "GameRequestHandler.h"
 #include "LoggedUser.h"
 #include "MenuRequestHandler.h"
 #include "RequestHandlerFactory.h"
@@ -20,4 +22,9 @@ RoomAdminRequestHandler* RequestHandlerFactory::createRoomAdminRequestHandler(co
 RoomMemberRequestHandler* RequestHandlerFactory::createRoomMemberRequestHandler(const LoggedUser& user, Room room)
 {
     return new RoomMemberRequestHandler(user, std::move(room));
+}
+
+GameRequestHandler* RequestHandlerFactory::createGameRequestHandler(const LoggedUser& user, Game& room)
+{
+    return new GameRequestHandler(user, std::move(room));
 }
