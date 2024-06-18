a8b2563e50ffe47450e5257c57d3a01dfc12b36a
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-06-18 03:36:38 +0300

    implemented some more

--- Trivia/Infrastructure/RequestHandlerFactory.h
+++ Trivia/Infrastructure/RequestHandlerFactory.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Game.h"
+#include "GameRequestHandler.h"
 #include "LoggedUser.h"
 #include "MenuRequestHandler.h"
 #include "Room.h"
@@ -11,4 +13,5 @@ namespace RequestHandlerFactory
 	MenuRequestHandler* createMenuRequestHandler(const LoggedUser& user);
 	RoomAdminRequestHandler* createRoomAdminRequestHandler(const LoggedUser& user, Room room);
 	RoomMemberRequestHandler* createRoomMemberRequestHandler(const LoggedUser& user, Room room);
+	GameRequestHandler* createGameRequestHandler(const LoggedUser& user, Game& room);
 }; // namespace RequestHandlerFactory
