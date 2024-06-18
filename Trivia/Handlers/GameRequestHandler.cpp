f3b692c002c49d3a0d5768ffe89b4a86397ad906
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-06-18 04:13:26 +0300

    compiles without warnings, need to implement each of GameRequestHandler's private functions

--- Trivia/Handlers/GameRequestHandler.cpp
+++ Trivia/Handlers/GameRequestHandler.cpp
@@ -13,7 +13,7 @@
 #endif
 
 
-GameRequestHandler::GameRequestHandler(LoggedUser user, Game& game) noexcept :
+GameRequestHandler::GameRequestHandler(const LoggedUser user, Game& game) noexcept :
     m_game(game),
     m_user(std::move(user))
 {}
@@ -36,7 +36,27 @@ RequestResult GameRequestHandler::handleRequest(const RequestInfo& info) noexcep
 {
     try
     {
+        switch (info.id)
+        {
+        //case GET_QUESTION:
+        //    return this->getQuestion(info);
+        //    //break;
 
+        //case SUBMIT_ANSWER:
+        //    return this->submitAnswer(info);
+        //    //break;
+
+        //case GET_GAME_RESULTS:
+        //    return this->getGameResults(info);
+        //    //break;
+
+        //case LEAVE_GAME:
+        //    return this->leaveGame(info);
+        //    //break;
+
+        default: // This should not happen
+            throw ServerException("Request is not relevant to GameRequestHandler!");
+        }
     }
     catch (const ServerException& e)
     {
