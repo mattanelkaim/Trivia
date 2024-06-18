7d7a4c3cce3ce15058b6f7809e58858db40b68b7
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-06-19 00:35:33 +0300

    implemented getQuestionForUser

--- Trivia/Managers/Game.cpp
+++ Trivia/Managers/Game.cpp
@@ -1,9 +1,11 @@
 #include "Game.h"
 #include "LoggedUser.h"
 #include "NotFoundException.h"
+#include "Question.h"
 #include "RoomManager.h"
 #include "ServerDefinitions.h"
 #include <cstdint>
+#include <optional>
 #include <stdexcept>
 #include <vector>
 
@@ -16,7 +18,7 @@ uint8_t Game::submitAnswer(const LoggedUser& user, uint8_t answerId)
 {
     try
     {
-        const uint8_t correctAnswerId = this->m_players.at(user).currentQuestion.getCorrectAnswerId();
+        const uint8_t correctAnswerId = this->m_players.at(user).currentQuestion->getCorrectAnswerId();
 
         if (answerId == correctAnswerId)
             this->m_players.at(user).correctAnswerCount++;
@@ -35,6 +37,23 @@ void Game::removePlayer(const LoggedUser& user)
 {
     RoomManager::getInstance().getRoom(this->m_gameId).removeUser(user);
     this->m_players.erase(user);
+}
+
+std::optional<Question> Game::getQuestionForUser(const LoggedUser& user) noexcept
+{
+    try
+    {
+        if (++(this->m_players.at(user).currentQuestion) == m_questions.cend()) [[unlikely]]
+        {
+            return std::nullopt;
+        }
+
+        return *(this->m_players.at(user).currentQuestion); // Dereference the iterator
+    }
+    catch (const std::out_of_range&) // If user does not exist for some reason
+    {
+        return std::nullopt;
+    }
 }
 
 // NOLINTNEXTLINE(bugprone-exception-escape) - ignore std::bad_alloc
